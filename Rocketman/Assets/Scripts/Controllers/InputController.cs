a0c6eba0fa63cde566bfe883d82aad6d268f3b16
gulersuleyman <ggulersuleyman@gmail.com>
2021-12-29 21:30:28 +0300

    Added GameManager and InputController
    Objeler yuklendi. GameManager ve InputController scriptleri olusturuldu.

---
+++ Rocketman/Assets/Scripts/Controllers/InputController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController
+{
+    public bool FirstMouseClick => Input.GetMouseButtonDown(0);
+    public bool MouseClick => Input.GetMouseButton(0);
+    public bool MouseUp => Input.GetMouseButtonUp(0);
+}
---
+++ Rocketman/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        SingletonThisGameObject();
+    }
+    public void SingletonThisGameObject()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
