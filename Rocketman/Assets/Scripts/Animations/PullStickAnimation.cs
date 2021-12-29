f640474808df1df97fe3f0ec9ed8b03eaed74e25
gulersuleyman <ggulersuleyman@gmail.com>
2021-12-30 01:58:01 +0300

    Stick Animation
    Stick animator controller olusturuldu ve baslangictaki cubuk hareketi icin
    PullStickAnimation scripti olusturuldu.

---
+++ Rocketman/Assets/Scripts/Animations/PullStickAnimation.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullStickAnimation : MonoBehaviour
+{
+    Animator _animator;
+    InputController _input;
+
+
+    bool _isBeginning;
+    bool _release;
+    float _firstTouch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _input = new InputController();
+
+        _isBeginning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isBeginning)
+        {
+            
+            float currentPosition;
+            float distance;
+
+            if (_input.FirstMouseClick)
+            {
+                _firstTouch = Input.mousePosition.x;
+            }
+            if (_input.MouseClick)
+            {
+                currentPosition = Input.mousePosition.x;
+                distance = (_firstTouch - currentPosition)/100;
+                _animator.SetFloat("Pull", distance);
+            }
+            if(_input.MouseUp)
+            {
+                _release = true;
+                ReleaseAnimation(_release);
+                _isBeginning = false;
+            }
+        }
+    }
+
+    public void ReleaseAnimation(bool isReleasing)
+    {
+        if (isReleasing == _animator.GetBool("Release")) return;
+
+        _animator.SetBool("Release", isReleasing);
+    }
+}
